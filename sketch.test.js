6f737c461bb5c311878bbf646c371d93bbb6b33c
Nguyen Viet Hung <viethungax@gmail.com>
2018-02-03 00:06:27 +0700

    Added test for multiply function

--- sketch.test.js
+++ sketch.test.js
@@ -1,4 +1,4 @@
-const sum = require('./sketch');
+const { sum, multiply } = require('./sketch');
 
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(sum(1, 2)).toBe(3);
@@ -8,4 +8,10 @@ test('adds 1 + 2 to equal 3', sumTest);
 
 function sumTest() {
   expect(sum(1, 2)).toBe(3);
+}
+
+test('adds 1 * 2 to equal 2', multiplyTest);
+
+function sumTest() {
+  expect(multiply(1, 2)).toBe(2);
 }
